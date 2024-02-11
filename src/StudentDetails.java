d01a973cd6eb2e08cff83170fc03c3fd72dfa872
Anilyadav0827 <anilyadavmekala6@gmail.com>
2024-02-11 11:23:12 +0530

    gitignored and added student

---
+++ src/StudentDetails.java
@@ -0,0 +1,9 @@
+public class StudentDetails {
+    private String name;
+    private String fatherName;
+
+    public StudentDetails(String name, String fatherName) {
+        this.name = name;
+        this.fatherName = fatherName;
+    }
+}
